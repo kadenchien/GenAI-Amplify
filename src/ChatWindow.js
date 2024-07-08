695d1b4b150d69fdb69620e506e430a66e653b10
kadenchien <kaden.chien@gmail.com>
2024-07-08 12:48:08 -0400

    added images and changed chatwindow css

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -7,7 +7,7 @@ import { faCopy } from '@fortawesome/free-regular-svg-icons';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
 const ChatWindow = () => {
-  const options = ['Titan', 'Mistral', 'Cohere', 'Stability', 'Jurassic', 'LLaMA-3'];
+  const options = ['Titan', 'Mistral', 'Cohere', 'Stability', 'Claude', 'LLaMA-3'];
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
   const [selectedOption, setSelectedOption] = useState(options[0]);
