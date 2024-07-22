47dc741659025a4aa32e4bfb1b86fb8b1f00d8bf
kadenchien <kaden.chien@gmail.com>
2024-07-22 10:11:02 -0400

    added dropdown locking and header image display

--- src/DropDown.js
+++ src/DropDown.js
@@ -5,17 +5,24 @@ import './DropDown.css';
 const DropDown = ({ options, onSelect }) => {
   const [selected, setSelected] = useState('Choose Model');
   const [isOpen, setIsOpen] = useState(false);
+  const [isLocked, setIsLocked] = useState(false);
 
   const handleSelect = (option) => {
     setSelected(option);
     setIsOpen(false);
+    setIsLocked(true); // Lock the dropdown after first selection
     onSelect(option);
   };
+  const selectedIndex = options.indexOf(selected);
+  const imgSrc = `/images/${selectedIndex + 1}.png`;
 
   return (
     <div className="dropdown">
-      <div className="dropdown-header" onClick={() => setIsOpen(!isOpen)}>
-        {selected}
+      <div className={`dropdown-header ${isLocked ? 'locked' : ''}`} onClick={() => !isLocked && setIsOpen(!isOpen)}>
+        <div className ="dropdown-header-content">
+            {isLocked && <img src={imgSrc} className="dropdown-image" alt="selected model" />} {/* Conditionally render image */}
+            {selected}
+        </div>
         <span className={`arrow ${isOpen ? 'open' : ''}`}>▼</span>
       </div>
       {isOpen && (
